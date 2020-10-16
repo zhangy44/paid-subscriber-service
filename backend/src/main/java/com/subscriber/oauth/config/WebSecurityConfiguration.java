efecd7bd6b5cc317a4e4da38892453c546d5f4bf
zhangy44 <clare_shiki@outlook.com>
2020-10-16 18:02:48 -0400

    web security config - password encoder

---
+++ backend/src/main/java/com/subscriber/oauth/config/WebSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.subscriber.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
